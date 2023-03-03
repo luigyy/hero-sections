061c667aa0cc633842b36e467df6f82a74306843
luigyy <luigy.valverde@ucr.ac.cr>
2023-03-02 19:26:59 -0600

    added navbar

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,5 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, FC } from "react";
 import styles from "@/styles/Home.module.css";
+
+const Navbars: FC<{ creep: boolean }> = ({ creep }) => {
+  return (
+    <>
+      {creep ? (
+        <div className="text-2xl md:w-[98vw] mx-[1vw] mt-1  border-4 border-white absolute top-0">
+          <div className="flex justify-around py-3 md:mx-10">
+            <span className="text-3xl font-creep">MyLogo</span>{" "}
+            <div className="flex gap-5">
+              <a>Products</a>
+              <a>About</a>
+              <a>Contact</a>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="text-2xl md:w-[98vw] mx-[1vw] mt-1 border-4 border-black absolute top-0">
+          <div className="flex justify-around py-3 md:mx-10">
+            <span className="text-3xl font-fredoka">MyLogo</span>{" "}
+            <div className="flex gap-5">
+              <a>Products</a>
+              <a>About</a>
+              <a>Contact</a>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 interface page1Props {}
 
@@ -26,6 +56,7 @@ const page1: React.FC<page1Props> = ({}) => {
       <div
         className={`h-screen  bg-black text-white  font-semibold text-6xl md:text-9xl grid items-center text-center overflow-hidden absolute w-full`}
       >
+        <Navbars creep={true} />
         <h1 className=" w-[70vw] mx-[15vw]">
           This is gonna be <br /> <span className="font-creep">aweful</span>
         </h1>
@@ -34,6 +65,7 @@ const page1: React.FC<page1Props> = ({}) => {
         style={{ width: `${mousePos}%` }}
         className={`h-screen bg-yellow-300 text-black  font-semibold text-6xl md:text-9xl grid items-center text-center overflow-hidden absolute w-full `}
       >
+        <Navbars creep={false} />
         <h1 className="w-[70vw] mx-[15vw]">
           This is gonna be <br /> <span className="font-fredoka">awsome</span>
         </h1>
